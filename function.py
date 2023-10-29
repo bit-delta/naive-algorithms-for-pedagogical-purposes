55d243aed8d4c6c5fd5b5f68b29191cb395cd973
bit-delta <gregory.m.fields@gmail.com>
2023-10-29 15:01:28 -0400

    derivative returns a Function, lazy-loading

--- function.py
+++ function.py
@@ -8,21 +8,26 @@ class Function:
             self.exponents = exponents
         else:
             raise TypeError("Function.exponents must be a list.")
+        self._derivative = None
 
     # Simplest form of derivative. Single-variable linear equation with no chain rule or special cases (e.g. e^x)
+    @property
     def derivative(self):
-        if len(self.coefficients) != len(self.exponents):
-            raise Exception("coefficients: list, exponents: list must be the same length.")
-        new_coefficients = []
-        new_exponents = []
-        for i in range(len(self.coefficients)):
-            new_coefficients.append(self.exponents[i] * self.coefficients[i])
-            new_exponents.append(self.exponents[i] - 1)
-        return [new_coefficients, new_exponents]
+        if self._derivative is None:
+            if len(self.coefficients) != len(self.exponents):
+                raise Exception("coefficients: list, exponents: list must be the same length.")
+            new_coefficients = []
+            new_exponents = []
+            for i in range(len(self.coefficients)):
+                new_coefficients.append(self.exponents[i] * self.coefficients[i])
+                new_exponents.append(self.exponents[i] - 1)
+            self._derivative = Function(new_coefficients, new_exponents)
+        return self._derivative
 
 
 my_func = Function([1, 2, 3], [4, 5, 6])
-my_func_derivative = my_func.derivative()
+# my_func_derivative = my_func.derivative()
 
-print(my_func.coefficients,my_func.exponents)
-print(my_func_derivative)
+print(my_func.coefficients, my_func.exponents)
+print(vars(my_func.derivative))
+# print(my_func_derivative)

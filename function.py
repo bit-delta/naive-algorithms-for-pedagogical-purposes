2cd8c36fff9ac0065474fe55b3e2739e96e1b605
bit-delta <148971548+bit-delta@users.noreply.github.com>
2023-10-30 07:20:51 -0400

    defines __str__ for Function class printing

--- function.py
+++ function.py
@@ -10,6 +10,9 @@ class Function:
             raise TypeError("Function.exponents must be a list.")
         self._derivative = None
 
+    def __str__(self):
+        return 'f(x) = ' + ' + '.join([str(c) + "x^" + str(e) for c, e in zip(self.coefficients, self.exponents)])
+
     # Simplest form of derivative. Single-variable linear equation with no chain rule or special cases (e.g. e^x)
     @property
     def derivative(self):
@@ -25,9 +28,11 @@ class Function:
         return self._derivative
 
 
-my_func = Function([1, 2, 3], [4, 5, 6])
+my_func = Function([10, 12, 2, 0, 1, 4], [0, 1, 2, 3, 4, 5])
 # my_func_derivative = my_func.derivative()
 
 print(my_func.coefficients, my_func.exponents)
 print(vars(my_func.derivative))
-# print(my_func_derivative)
+print(my_func)
+
+# TODO: Taylor Series function like taylor(function, n) returns nth degree Taylor series
